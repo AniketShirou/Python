18fec77e25988c9752e5bc8bacf66285f1827938
Aniket Rana <121351449+AniketShirou@users.noreply.github.com>
2025-09-08 01:34:50 +0530

    Add files via upload

---
+++ Chapter1/module.py
@@ -0,0 +1,10 @@
+import pyjokes
+
+joke = pyjokes.get_joke()
+print(joke)
+
+
+# multiple line comments -> ''' or """
+"""
+  Thanks , for the chapter 1;
+"""
---
+++ Chapter1/table5.py
@@ -0,0 +1,4 @@
+n = int(input())
+
+for i in range(1, 11):
+    print(f'5 x {i} = ', n*i)
